52b723d9a3ebcc827469aa1f68b5e31b0c8e9617
Kevin Harp <kharp1984@gmail.com>
2023-06-14 21:10:40 -0400

    routine_activities ajax requests done

--- src/ajax-requests/routine_activities.js
+++ src/ajax-requests/routine_activities.js
@@ -1,3 +1,39 @@
 export const BASE_URL = 'http://fitnesstrac-kr.herokuapp.com/api/';
 
 //* ROUTINE ACTIVITIES
+
+export const updateRoutineActivity = async (routineActivityId, token, routineActivity) => {
+    try {
+      const response = await fetch(`${BASE_URL}/routine_activities/:${routineActivityId}`, {
+        method: "PATCH",
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`
+        },
+        body: JSON.stringify({
+          routineActivity
+        })
+      });
+      const result = await response.json();
+      return result
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  export const deletePost = async (routineActivityId, token) => {
+    try {
+      const response = await fetch(`${BASE_URL}/routine_activities/:${routineActivityId}`, {
+        method: "DELETE",
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`
+        }
+      });
+      const result = await response.json();
+      return result
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  
